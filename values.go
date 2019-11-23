99e5454593c39c9d2798af6cf88e846b5a0e3c8a
CryptoJones <cryptojones@gmail.com>
2019-11-23 17:56:36 -0600

    Values

--- values.go
+++ values.go
@@ -5,4 +5,8 @@ import "fmt"
 func main() {
 	fmt.Println("go" + "lang")
 	fmt.Println("1 + 1 = ", 1+1)
+	fmt.Println("5.0/2", 5.0/2)
+	fmt.Println(true && true)
+	fmt.Println(true || false)
+	fmt.Println(!true)
 }
